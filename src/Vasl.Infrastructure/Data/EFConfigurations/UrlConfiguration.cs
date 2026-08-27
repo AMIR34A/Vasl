eaac081a2523b7a5269702a1c20190fa21234baa
Amirreza <83154350+AMIR34A@users.noreply.github.com>
2026-08-27 10:35:21 +0330

    -Add DbContext. -Add url's configurations. -Add packages into the Infra layer..

---
+++ src/Vasl.Infrastructure/Data/EFConfigurations/UrlConfiguration.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Vasl.Domain.Entities;
+
+namespace Vasl.Infrastructure.Data.EFConfigurations;
+
+public class UrlConfiguration : IEntityTypeConfiguration<Url>
+{
+    public void Configure(EntityTypeBuilder<Url> builder)
+    {
+        builder.HasKey(entity => entity.Id);
+
+        builder.Property(entity => entity.Id)
+               .ValueGeneratedNever();
+
+        builder.Property(entity => entity.Code)
+               .IsRequired()
+               .HasConversion<string>()
+               .HasMaxLength(20);
+
+        builder.Property(entity => entity.OriginalUrl)
+               .IsRequired()
+               .HasConversion<string>()
+               .HasMaxLength(2000)
+               .IsUnicode();
+
+        builder.Property(entity => entity.CreationTimeUtc)
+               .IsRequired();
+
+        builder.Property(entity => entity.ExpirationTimeUtc)
+               .IsRequired(false);
+
+        builder.HasIndex(entity => entity.Code);
+    }
+}
---
+++ src/Vasl.Infrastructure/Data/VaslDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Vasl.Domain.Entities;
+
+namespace Vasl.Infrastructure.Data;
+
+public class VaslDbContext : DbContext
+{
+    public DbSet<Url> Urls { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        base.OnConfiguring(optionsBuilder);
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(GetType().Assembly);
+        base.OnModelCreating(modelBuilder);
+    }
+}
