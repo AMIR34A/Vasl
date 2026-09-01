a0b9d0a61b518bdc460c5bea4aea9de7e35d4ee4
Amirreza <83154350+AMIR34A@users.noreply.github.com>
2026-09-01 11:14:19 +0330

    Add Read and Write Endpoints

---
+++ src/Vasl.WebAPI/Endpoints/Read.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using Vasl.ApplicationService.Urls.Queries.Get;
+
+namespace Vasl.WebAPI.Endpoints;
+
+public static class Read
+{
+    public static void AddReadEndpoints(this IEndpointRouteBuilder builder)
+    {
+        builder.MapGet("/{code}", async ([FromRoute] string code,
+            IMediator mediator,
+            CancellationToken cancellationToken) =>
+        {
+            var result = await mediator.Send(new GetUrlQuery(code), cancellationToken);
+            return result.OriginalUrl.IsNullOrEmpty() ? Results.NotFound() : Results.Redirect(result.OriginalUrl);
+        });
+    }
+}
---
+++ src/Vasl.WebAPI/Endpoints/Write.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using Microsoft.Extensions.Options;
+using Vasl.ApplicationService.Urls.Commands.Create;
+using Vasl.Infrastructure;
+
+namespace Vasl.WebAPI.Endpoints;
+
+public static class Write
+{
+    public static void AddWriteEndpoints(this IEndpointRouteBuilder builder)
+    {
+        builder.MapPost("/CreateShortUrl", async (CreateUrlCommand createUrl,
+            IMediator mediator,
+            IOptions<AppSettings> options,
+            CancellationToken cancellationToken) =>
+        {
+            var result = await mediator.Send(createUrl, cancellationToken);
+            return Results.Ok(new
+            {
+                Code = result.Code,
+                Url = $"{options.Value.RedirectUrl}/{result.Code}"
+            });
+        });
+    }
+}
