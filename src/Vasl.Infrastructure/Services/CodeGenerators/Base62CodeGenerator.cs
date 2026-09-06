bcf1c2141ac78f5717a08ff9f35e38c9b9b174a2
Amirreza <83154350+AMIR34A@users.noreply.github.com>
2026-09-06 17:41:09 +0330

    Change code generator.

--- src/Vasl.Infrastructure/Services/CodeGenerators/Base62CodeGenerator.cs
+++ src/Vasl.Infrastructure/Services/CodeGenerators/Base62CodeGenerator.cs
@@ -6,13 +6,12 @@ namespace Vasl.Infrastructure.Services.CodeGenerators;
 public class Base62CodeGenerator : ICodeGenerator
 {
     private const int Base = 62;
-    private const long Modulus = 1L << 40;
-    private const long Prime = 3_935_559_000_370_003_845;
+    private const long Offset = 1_000_000;
     private const string Alphabet = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
     public string GenerateCode(long id)
     {
-        long scrambled = (id * Prime) % Modulus;
+        long scrambled = id + Offset;
         return Base62Raw(scrambled);
     }
 
